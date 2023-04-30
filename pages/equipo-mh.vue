5e7b6854a5b94d79207e28c1935905a0fb851fd0
diego048 <103468763+diego048@users.noreply.github.com>
2023-04-30 01:24:49 -0500

    Update equipo-mh.vue

--- pages/equipo-mh.vue
+++ pages/equipo-mh.vue
@@ -279,7 +279,7 @@ export default {
 .card1 {
   background-color: #fff;
   border-radius: 4px;
-  box-shadow: 5px 10px 10px 1px rgba(0, 0, 0, 0.2);
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
   overflow-x: hidden;
   position: relative;
 }
