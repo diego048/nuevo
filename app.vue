909cb52966b9721cd60e9f41e373817f22c2fbd2
diego048 <103468763+diego048@users.noreply.github.com>
2023-04-30 13:28:57 -0500

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-screen">
-    <div v-if="loading" class="fixed left-0 top-0 h-0.5 w-full z-50 bg-green-500" style="z-index: 10000" />
+    <div v-if="true" class="fixed left-0 top-0 h-0.5 w-full z-50 bg-green-500" style="z-index: 10000" />
     <NuxtLayout style="z-index: 999">
       <NuxtPage />
     </NuxtLayout>
