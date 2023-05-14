98b4269aad61cb3069ab550eccb1e3eccea074ba
diego048 <103468763+diego048@users.noreply.github.com>
2023-05-13 23:17:25 -0500

    Update login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -91,7 +91,7 @@ export default {
   methods: {
     async log1() {
       let result = await axios
-        .post("http://3.232.232.195:5000/login", {
+        .post("https://pagina1.onrender.com/login", {
           username: this.username,
           password: this.password,
         })
