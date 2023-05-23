4d187efe5b9b3eecdc6c65adc6eee6038dee4131
diego048 <103468763+diego048@users.noreply.github.com>
2023-05-23 01:51:15 -0500

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,51 +1,13 @@
 <template>
   <div>
-    <div v-if="loading" class="spinner-wrapper">
-       <div class="spinner-border" role="status">
-         <span class="visually-hidden">Loading...</span>
-       </div>
-     </div>
-    <NuxtLayout style="z-index: -1">
+    <NuxtLayout>
+      <NuxtLoadingIndicator/>
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
 
-<script setup lang="ts">
-  const nuxtApp = useNuxtApp();
-  const loading = ref(false);
-  nuxtApp.hook("page:start", () => {
-    loading.value = true;
-  });
-  nuxtApp.hook("page:finish", () => {
-    loading.value = false;
-  });
-  useHead({
-  link: [
-    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-    {
-      href:"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
-      rel:"stylesheet", 
-      integrity:"sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65",
-      crossorigin:"anonymous"
-     },
-     {
-       rel:"stylesheet",
-       href:"https://cdn.materialdesignicons.com/6.4.95/css/materialdesignicons.min.css"
-     },
-     {
-       rel:"stylesheet",
-       href:"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css"
-     },
-    ],
-     script: [
-      {
-        src:"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", 
-        integrity:"sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4", 
-        crossorigin:"anonymous"
-       },
-     ]
-  })
+<script setup lang="es">
 </script>
 <style>
 .page-enter-active,
