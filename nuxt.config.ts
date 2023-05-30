28924d3a83d710b767eeb65693f4dd00ab314e99
diego048 <103468763+diego048@users.noreply.github.com>
2023-05-30 18:33:11 -0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const { __dirname } = createCommonJS(import.meta.url)
 export default defineNuxtConfig({
     hooks: {
     'pages:extend' (pages) {
