81f5816155791e71912bcdf52f43369967a20f17
diego048 <103468763+diego048@users.noreply.github.com>
2023-05-30 21:40:06 -0500

    Update navigation.vue

--- components/navigation.vue
+++ components/navigation.vue
@@ -247,3 +247,14 @@ export default defineComponent({
 },
 })
 </script>
+<script setup>
+useHead({
+   script: [
+      {
+         src:"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", 
+         integrity:"sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4", 
+         crossorigin:"anonymous"
+       },
+      ]
+})
+</script>
