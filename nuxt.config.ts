6341f6eb39253414df6ae046c88122837d77c3cd
diego048 <103468763+diego048@users.noreply.github.com>
2023-06-01 18:03:01 -0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
         head: {
             charset: 'utf-16',
             viewport: 'width=device-width, initial-scale=1',
-            title: 'LogicorPeru',
+            title: 'Logicorp',
             meta: [
               { name: 'description', content: 'Somos un grupo de profesionales especializados en almacenes, contamos con más de 20 años' }
             ],
